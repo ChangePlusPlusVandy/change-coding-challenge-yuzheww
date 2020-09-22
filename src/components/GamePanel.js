3bb60b1e47c722b84acf4088d8b16d8c8203a81a
yuzheww <yuzhe.wang@vanderbilt.edu>
2020-09-23 00:07:18 +0800

    modify game precedure

--- src/components/GamePanel.js
+++ src/components/GamePanel.js
@@ -7,11 +7,13 @@ class GamePanel extends Component {
     this.state = {
       error: null,
       correct: false,
-      answer: '',
+      answer: "",
       NumQ: 10,
       tweet: "",
       text1: [],
       text2: [],
+      click1: false,
+      click2: false,
     };
   }
 
@@ -24,56 +26,50 @@ class GamePanel extends Component {
 
   startGame = () => {
     this.props.zeroNum();
-    let btn1 = document.getElementById('btn1');
-    let btn2 = document.getElementById('btn2');
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    let correctAnswer = this.state.answer;
     for (let i = 0; i < this.state.NumQ; ++i) {
-      let choice = Math.random();
-      if (choice < 0.5) {
-        this.setState({
-          tweet: this.state.text1[
-            Math.floor(Math.random() * this.state.text1.length)
-          ],
-          answer: "kanyewest",
-        });
-        while(btn1.clicked === false && btn2.clicked === false){
-            if(btn2.clicked){
-                this.setState({
-                    answer: true,
-                });
-            }else{
-                this.setState({
-                    answer: false,
-                });
-            }
-        }
-      } else {
-        this.setState({
-          tweet: this.state.text2[
-            Math.floor(Math.random() * this.state.text2.length)
-          ],
-          answer: "elonmusk",
-        });
-        while(btn1.clicked === false && btn2.clicked === false){
-            if(btn2.clicked){
-                this.setState({
-                    answer: true,
-                });
-            }else{
-                this.setState({
-                    answer: true
-                });
-            }
+      this.renderTweet();
+      while (btn1.clicked === false && btn2.clicked === false) {
+        if (btn1.clicked && correctAnswer === "kanyewest") {
+          this.setState({
+            correct: true,
+          });
+        } else if(btn2.clicked && correctAnswer === "elonmusk"){
+          this.setState({
+            correct: true,
+          });
         }
       }
-
     }
   };
 
+  renderTweet = () => {
+    let choice = Math.random();
+    if (choice < 0.5) {
+      this.setState({
+        tweet: this.state.text1[
+          Math.floor(Math.random() * this.state.text1.length)
+        ],
+        answer: "kanyewest",
+      });
+    } else {
+      this.setState({
+        tweet: this.state.text2[
+          Math.floor(Math.random() * this.state.text2.length)
+        ],
+        answer: "elonmusk",
+      });
+    }
+  };
 
   componentDidMount() {
     fetch(TWITTER_BASE_URL + "&screen_name=kanyewest", {
+      method: 'GET', 
+      mode: 'no-cors',
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        'Authorization': `Bearer ${BEARER_TOKEN}`,
       },
     })
       .then((res) => res.json())
@@ -91,8 +87,10 @@ class GamePanel extends Component {
       );
 
     fetch(TWITTER_BASE_URL + "&screen_name=elonmusk", {
+      method: 'GET', 
+      mode: 'no-cors',
       headers: {
-        Authorization: `Bearer ${BEARER_TOKEN}`,
+        'Authorization': `Bearer ${BEARER_TOKEN}`,
       },
     })
       .then((res) => res.json())
@@ -132,16 +130,16 @@ class GamePanel extends Component {
         <div>
           <Button
             className="guess-btn"
-            id = "btn1"
-            size= "large"
+            id="btn1"
+            size="large"
             onClick={() => this.checkCelebrity1}
           >
             Kanye West
           </Button>
           <Button
             className="guess-btn"
-            id = "btn2"
-            size = "large"
+            id="btn2"
+            size="large"
             onClick={() => this.checkCelebrity1}
           >
             Elon Musk
--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const KANYE_BASE_URL = "https://api.twitter.com/1.1/statuses/user_timelin
 
 export const ELON_BASE_URL = "https://api.twitter.com/1.1/statuses/user_timeline.json?exclude_replies=true&include_rts=false&screen_name=elonmusk";
 
-export const TWITTER_BASE_URL = "https://api.twitter.com/1.1/statuses/user_timeline.json?exclude_replies=true&include_rts=false&screen_name=elonmusk";
+export const TWITTER_BASE_URL = "https://api.twitter.com/1.1/statuses/user_timeline.json?exclude_replies=true&include_rts=false";
 
 export const TWITTER_API_KEY = "PDmm1B9AdxU7DFCbPhN6LCSjB";
 
